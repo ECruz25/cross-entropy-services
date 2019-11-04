3d0141be120514230bbe4b8102f6f79bec6a780d
edwinscruz <escruz@caracolknits.com>
2019-11-04 12:32:42 -0600

    testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from app.inventory_demand import transform_data, train_model
+# from app.inventory_demand import transform_data, train_model
 from flask import Flask, jsonify
 from flask import request
 from flask_cors import CORS
@@ -9,12 +9,12 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/api/inventory-demand/data-transformation", methods=['POST'])
-def inventory_demand_training():
-    content = request.get_json()
-    df = pd.io.json.json_normalize(content, 'data')
-    months_to_predict = content['monthsToPredict']
-    return transform_data(df, months_to_predict)
+# @app.route("/api/inventory-demand/data-transformation", methods=['POST'])
+# def inventory_demand_training():
+#     content = request.get_json()
+#     df = pd.io.json.json_normalize(content, 'data')
+#     months_to_predict = content['monthsToPredict']
+#     return transform_data(df, months_to_predict)
 
 
 # @app.route("/api/inventory-demand/model-training", methods=['POST'])
