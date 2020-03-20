7f9335b56a89a94fabc56162d163a3590210e6de
Edwin <edwincruz25@unitec.edu>
2020-03-20 09:04:40 -0600

    .

--- app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-# import pyodbc
-
-print('hi')
-
-if __name__ == '__main__':
-    app.run(debug=True)
--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-    JWT_SECRET_KEY = 'test123'
-    JWT_ALGORITHM = 'HS256'
-    JWT_AUTH_ENDPOINT = 'jwt'
-    JWT_AUTH_USERNAME_KEY = 'username'
-    JWT_AUTH_PASSWORD_KEY = 'password'
